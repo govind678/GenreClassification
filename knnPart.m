e6b8ab67dbd30955397c2ead6ae90c5dbeed6c1f
Govinda Ram Pingali <govind678@gmail.com>
2013-09-24 20:46:55 -0400

    Cleaning Up Again

--- knnPart.m
+++
@@ -1,9 +0,0 @@
-load 'Results/PostScatter';
-
-testGenre = 4;
-testSong = 51;
-k = 5;
-
-class = knnClassifier(normalMatrix, normalMatrix(testGenre,testSong,:),k);
-
-disp(sprintf('Genre of test song is: %d',class));
